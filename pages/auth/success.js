92c0f4e4f17f922f901348cf4c68a70ecee571d6
Matteo Gassend <matteo1.gassend@epitech.eu>
2022-04-08 02:03:15 +0200

    add: auth callback pages

---
+++ pages/auth/success.js
@@ -0,0 +1,29 @@
+import { useRouter } from "next/router"
+import { useEffect } from "react"
+import toast from "react-hot-toast"
+import { useRecoilState } from "recoil"
+import userAtom from "../../context/atoms/userAtom"
+import { client } from "../../utils/client"
+
+
+export function SuccessAuth() {
+  const router = useRouter()
+  const [user, setUser] = useRecoilState(userAtom)
+  useEffect(() => {
+    const getUser = async() => {
+      try {
+        const u = await client.account.get()
+        setUser(u)
+      } catch (error) {
+        toast.error("something went wrong")
+        console.error(error)
+      }
+    }
+    getUser()
+  }, [router, setUser])
+  return (
+    <div>
+
+    </div>
+  )
+}
