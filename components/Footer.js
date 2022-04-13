621e0d3647ab89d0f11d0bf2b8e23a63aeae5f69
Matteo Gassend <matteo1.gassend@epitech.eu>
2022-04-13 14:28:30 +0200

    add: light/dark mode switch

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,27 @@
+import { useTheme } from "next-themes";
+import Moon from "../icons/Moon";
+import Sun from "../icons/Sun";
+
 export default function Footer() {
+
+  const { theme, setTheme } = useTheme()
+
   return (
-    <div>
-      &copy; {new Date().getFullYear()} Matteo Gassend
+    <div className="flex justify-between items-center">
+      <span>
+
+        &copy; {new Date().getFullYear()} Matteo Gassend
+      </span>
+      <div>
+        {theme === "light" && <Moon className="w-6 h-6" onClick={() => {
+          setTheme("dark")
+        }} />}
+        {theme === "dark" && <Sun className="w-6 h-6" onClick={() => {
+          setTheme("light")
+        }} />}
+
+
+      </div>
     </div>
   )
 }
