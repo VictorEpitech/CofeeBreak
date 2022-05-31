b3c89a2ab12b321fe693c217ee5ddc457f6a2dac
Matteo Gassend <mgassend@gmail.com>
2022-05-31 13:59:17 +0200

    add: new stat for tracked users

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -1,32 +1,52 @@
-import { useState, useEffect } from "react"
-import { useSetRecoilState } from "recoil"
-import loadingAtom from "../../context/atoms/loadingAtom"
-import { client } from "../../utils/client"
+import { useState, useEffect } from "react";
+import { useSetRecoilState } from "recoil";
+import loadingAtom from "../../context/atoms/loadingAtom";
+import { client } from "../../utils/client";
 
 export default function DashboardHome() {
-  const [fund, setFund] = useState(0)
-  const setLoading = useSetRecoilState(loadingAtom)
+  const [fund, setFund] = useState(0);
+  const [trackedUsers, setTrackedUsers] = useState(0);
+  const setLoading = useSetRecoilState(loadingAtom);
 
   useEffect(() => {
-    const getFunds = (async() => {
-      setLoading(true)
-      const fundData = await client.database.listDocuments(process.env.NEXT_PUBLIC_FUND_COLLECTION, undefined, 1, undefined, undefined, undefined, ["date"], ["DESC"])
+    const getFunds = async () => {
+      setLoading(true);
+      const fundData = await client.database.listDocuments(
+        process.env.NEXT_PUBLIC_FUND_COLLECTION,
+        undefined,
+        1,
+        undefined,
+        undefined,
+        undefined,
+        ["date"],
+        ["DESC"]
+      );
+      const trackedData = await client.database.listDocuments(process.env.NEXT_PUBLIC_CREDIT_COLLECTION)
       if (fundData.documents.length > 0) {
-        setFund(fundData.documents[0].totalAmount)
+        setFund(fundData.documents[0].totalAmount);
       }
+      setTrackedUsers(trackedData.total);
       setLoading(false);
-    })
-    getFunds()
-  }, [setLoading])
+    };
+    getFunds();
+  }, [setLoading]);
 
   return (
     <div className="w-full h-full">
-      <div className="stats shadow-xl">
-        <div className="stat">
-          <div className="stat-title">Funds</div>
-          <div className={`stat-value`}>{fund.toFixed(2)}</div>
+      <div className="w-full flex justify-evenly">
+        <div className="stats shadow-xl">
+          <div className="stat">
+            <div className="stat-title">Funds</div>
+            <div className={`stat-value`}>{fund.toFixed(2)}</div>
+          </div>
+        </div>
+        <div className="stats shadow-xl">
+          <div className="stat">
+            <div className="stat-title">Tracked Users</div>
+            <div className={`stat-value`}>{trackedUsers}</div>
+          </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
