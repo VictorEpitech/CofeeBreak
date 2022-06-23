dd0474fce9a293e471e58f846bba50b993b31041
Matteo Gassend <mgassend@gmail.com>
2022-06-23 09:55:18 +0200

    fix: require pay method if add in funds on charge

--- components/ChargesAdd.js
+++ components/ChargesAdd.js
@@ -1,22 +1,31 @@
 import {useState} from "react"
 import toast from "react-hot-toast";
+import { useRecoilValue } from "recoil";
+import payMethodsAtom from "../context/atoms/payMethods";
 import { client } from "../utils/client";
 
 export default function ChargesAdd({isOpen, setIsOpen, doc}) {
   const [value, setValue] = useState(0)
   const [inFunds, setInFunds] = useState(true)
+  const [payment, setPayment] = useState(undefined)
+  const payMethods = useRecoilValue(payMethodsAtom);
+
 
   const handleSubmit = async(e) => {
     e.preventDefault()
     const total = parseFloat(value) + parseFloat(doc.charges);
     try {
       console.log(doc)
+      if (inFunds && !payment) {
+        toast.error("please choose pay method");
+        return;
+      }
       await client.database.updateDocument(process.env.NEXT_PUBLIC_CREDIT_COLLECTION, doc["$id"], {email:  doc.email, charges: total});
       toast.success("charges updated")
       if (inFunds) {
-        const latestFund = await client.database.listDocuments(process.env.NEXT_PUBLIC_FUND_COLLECTION, undefined, 1, undefined, undefined, undefined, ["date"], ["DESC"]);
-        await client.database.createDocument(process.env.NEXT_PUBLIC_FUND_COLLECTION, "unique()", {amount: value * parseFloat(process.env.NEXT_PUBLIC_CHARGE_VALUE), reason: `${doc.email} recharge` || null, date: new Date().toISOString(), totalAmount: (latestFund.documents[0]?.totalAmount ?? 0) + (value * parseFloat(process.env.NEXT_PUBLIC_CHARGE_VALUE))});
-        toast.success("funds updated");
+          const latestFund = await client.database.listDocuments(process.env.NEXT_PUBLIC_FUND_COLLECTION, undefined, 1, undefined, undefined, undefined, ["date"], ["DESC"]);
+          await client.database.createDocument(process.env.NEXT_PUBLIC_FUND_COLLECTION, "unique()", {amount: value * parseFloat(process.env.NEXT_PUBLIC_CHARGE_VALUE), reason: `${doc.email} recharge` || null, date: new Date().toISOString(), totalAmount: (latestFund.documents[0]?.totalAmount ?? 0) + (value * parseFloat(process.env.NEXT_PUBLIC_CHARGE_VALUE)), method: payment});
+          toast.success("funds updated");
       }
       setIsOpen(false);
     } catch (error) {
@@ -39,6 +48,10 @@ export default function ChargesAdd({isOpen, setIsOpen, doc}) {
                   <input min={1} className="input input-bordered w-full max-w-xs" type="number" placeholder="Enter Amount" required value={value} onChange={(e) => setValue(e.target.value)}/>
                   <label htmlFor="funds">Add to funds ?</label>
                   <input name="funds" type="checkbox" checked={inFunds} onChange={() => setInFunds(!inFunds)} className="checkbox" />
+                  {inFunds &&                   <select className="select select-bordered w-full max-w-x" defaultValue="none" required onChange={(e) => setPayment(e.target.value)}>
+                    <option disabled value="none">Select payment method</option>
+                    {payMethods.map((e) => <option key={e.$id} value={e.$id}>{e.name}</option>)}
+                  </select>}
                 </div>
                 <div className="card-actions justify-end mt-3">
                   <button className="btn btn-error" onClick={() => {
