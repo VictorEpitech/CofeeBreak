d4cd07c60c0db4526d58c691780e9362618c0567
Matteo Gassend <matteo1.gassend@epitech.eu>
2022-10-31 09:00:52 +0100

    [fix] add default true to schema

--- src/schemas/charges.add.schema.js
+++ src/schemas/charges.add.schema.js
@@ -1,7 +1,7 @@
 import * as Yup from "yup";
 const AddChargeSchema = Yup.object().shape({
   charges: Yup.number().positive().required(),
-  addToFunds: Yup.boolean().required(),
+  addToFunds: Yup.boolean().required().default(true),
   payment_method: Yup.string().when("addToFunds", {
     is: true,
     then: Yup.string().required("please select a payment method"),
